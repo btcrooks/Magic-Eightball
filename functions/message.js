4df0cbaceb7cc1796657d96d51b3a18c62e7211a
Brandon Crooks <crookrb@hotmail.com>
2021-03-21 01:15:54 -0400

    refactor random response

--- functions/message.js
+++ functions/message.js
@@ -1,14 +1,11 @@
 const responses = require('../data/responses.json');
 
-const randomResponse = () => {
-  const numberOfResponses = responses.length;
-  const responseIndex = Math.floor(Math.random() * numberOfResponses);
-  return responses[responseIndex];
-}
+const randomize = (max) => Math.floor(Math.random() * max);
+const getRandomResponse = (data) => data[randomize(data.length)];
 
 exports.handler = async event => {
   return {
     statusCode: 200,
-    body: randomResponse(),
+    body: getRandomResponse(responses),
   }
 }
